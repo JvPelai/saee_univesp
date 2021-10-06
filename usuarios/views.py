f387e22f6297d3de449dd96bc2aff8ae4a50f8b3
JvPelai <jv.pelai@gmail.com>
2021-10-06 20:57:22 -0300

    carregar bibliotecas e arquivos no template, arquivo de dependencias e aplicativo de agendamentos

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "nome_pagina": "Dashboard",
+    }
+
+    return render(request, "index.html", context)
